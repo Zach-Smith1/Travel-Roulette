7918a683b7dd45c784c73259728c5cc832b7454a
Zach Smith <zachsmithglobal@icloud.com>
2022-08-10 14:29:52 -0600

    full functionality

--- src/App.js
+++ src/App.js
@@ -9,11 +9,12 @@ class App extends React.Component {
       lat: 15,
       lng: 40,
       zoom: 2,
-      current: null
+      current: null,
+      list: []
     }
   }
 
-  random = (e) => {
+  randomCity = (e) => {
   var num = Math.floor(Math.random() * 10) + 1;
   var latval = dest.Cities[num].lat;
   var lngval = dest.Cities[num].lng;
@@ -24,23 +25,90 @@ class App extends React.Component {
       zoom: 11,
       current: city
     })
-    console.log(this.state.current)
+  document.body.classList.toggle("top", true);
+  }
+  randomRegion = (e) => {
+    var num = Math.floor(Math.random() * 22) + 1;
+    var latval = dest.Regions[num].lat;
+    var lngval = dest.Regions[num].lng;
+    var area = dest.Regions[num].name;
+      this.setState({
+        lat: latval,
+        lng: lngval,
+        zoom: 8,
+        current: area
+      })
+    document.body.classList.toggle("top", true);
+    }
+
+  addPlace = (e) => {
+    var place = this.state.current;
+    var placeArray = this.state.list;
+    placeArray.push(place);
+    this.setState({
+      list: placeArray
+    })
+  }
+
+  clear = (e) => {
+    this.setState({
+      list: []
+    })
+  }
+
+  toggle = (e) => {
+    document.body.classList.toggle("dark");
+  }
+
+  listMaker = () => {
+    var resultList = [];
+    var list = this.state.list;
+    list.forEach((place) => {
+      resultList.push(<span id='listItem'>{place}<br/></span>)
+    });
+    return <div>{resultList}</div>;
   }
 
   render() {
+    if (this.state.current && this.state.list.indexOf(this.state.current) === - 1) {
+      var addIt = <button onClick={this.addPlace}>Add to Itineray!</button>;
+    } else {
+      var addIt = null;
+    }
+    if (this.state.list.length > 0) {
+      var bookIt = <a href='https://www.google.com/travel/flights'> Book it! </a>;
+      var clear = <button id='clear' onClick={this.clear}>Clear Itineray</button>;
+    } else {
+      var bookIt = null;
+      var clear = null;
+    }
     return (
       <div className='mainbox'>
         <h1>
-          Destination Roulette!
+          Travel Roulette!
         </h1>
-        <div className='rightbar' >Top 10 Cities to travel to from <a href='https://www.lonelyplanet.com/best-in-travel/cities'> Lonely Planet </a>
-        <br/><button id='button1' onClick={this.random}> Where Should I Go?? </button>
+        <div className='rightbar'>
+          <div>Top City Destinatinations<br/> from <a href='https://www.lonelyplanet.com/best-in-travel/cities'> Lonely Planet </a>
+        <br/></div>
+        <button id='button1' onClick={this.randomCity}> Find a City </button>
+        <div>Top Nature Destinatinations<br/> from <a href='https://www.nathab.com/blog/where-to-travel-in-2022/'> NatHab </a>
+        <br/></div>
+        <button id='button1' onClick={this.randomRegion}> Find a Region </button>
+        <span id='clear'>{clear}</span>
         </div>
-        <div id='location'>{this.state.current}</div>
+        <div id='location'>{this.state.current}<br/>
+        <span>{addIt}</span>
+        </div>
         <div className='mapbox'>
           <MyComponent lat={this.state.lat} lng={this.state.lng} zoom={this.state.zoom}/>
           </div>
-
+          <div className='farRight'>
+            <div className='itineray'>Itineray:<br/>
+            {this.listMaker()}
+            <span><br/>{bookIt}</span>
+            </div>
+            <button id='toggle' onClick={this.toggle}>Enjoy the View</button>
+          </div>
       </div>
     );
   }
--- src/destinations.js
+++ src/destinations.js
@@ -50,5 +50,117 @@ export default {
       lat: 35.8562,
       lng: 129.2247
     }
+  },
+  Regions: {
+    1: {
+      name: "Banff, Canada",
+      lat: 51.1784,
+      lng: -115.5708
+     },
+    2: {
+      name: "Queensland, Australia",
+      lat: -22.5752,
+      lng: 144.0848
+    },
+    3: {
+      name: "Mexico",
+      lat: 23.6345,
+      lng: -102.5528
+    },
+    4: {
+      name: "Sri Lanka",
+      lat: 7.8731,
+      lng: 80.7718
+    },
+    5: {
+      name: "Norway",
+      lat: 60.4720,
+      lng: 8.4689
+    },
+    6: {
+      name: "Slovenia",
+      lat: 46.1512,
+      lng: 14.9955
+    },
+    7: {
+      name: "Nepal",
+      lat: 28.3949,
+      lng: 84.1240
+    },
+    8: {
+      name: "Westfjords, Iceland",
+      lat: 65.9196,
+      lng: -21.8812
+    },
+    9: {
+      name: "Acadia National Park, Maine",
+      lat: 44.3386,
+      lng: -68.2733
+    },
+    10: {
+      name: "Galapagos Islands",
+      lat: -0.9538,
+      lng: -90.9656
+    },
+    11: {
+      name: "Porto, Portugal",
+      lat: 41.1579,
+      lng: -8.6291
+    },
+    12: {
+      name: "Namibia",
+      lat: -22.9576,
+      lng: 18.4904
+    },
+    13: {
+      name: "St. Moritz, Switzerland",
+      lat: 46.4908,
+      lng: 9.8355
+    },
+    14: {
+      name: "Colombia",
+      lat: 4.5709,
+      lng: -74.2973
+    },
+    15: {
+      name: "Alaska",
+      lat: 64.2008,
+      lng: -149.4937
+    },
+    16: {
+      name: "Antarctica",
+      lat: -72.8628,
+      lng: -93.11667382068542,
+    },
+    17: {
+      name: "Stewart Island, New Zealand",
+      lat: -47.0103,
+      lng: 167.8272
+    },
+    18: {
+      name: "Quebec, Canada",
+      lat: 46.8131,
+      lng: 71.2075
+    },
+    19: {
+      name: "Victoria, Australia ",
+      lat: -36.9848,
+      lng: 143.3906
+    },
+    20: {
+      name: "Costa Rica",
+      lat: 9.7489,
+      lng: -83.7534
+    },
+    21: {
+      name: "Ten Thousand Islands, Florida",
+      lat: 25.8587,
+      lng: -81.4817
+    },
+    22: {
+      name: "Yasuni National Park, Ecuador",
+      lat: -0.6562,
+      lng: -76.0705
+    }
   }
 }
