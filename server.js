aaa69d30d90f9662bd9c2fb6c98d3c4480bea473
Zach Smith <zachsmithglobal@icloud.com>
2022-08-10 19:31:25 -0600

    added server

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const compression = require('compression')
+
+
+//create server
+var app = express();
+//set port number
+var port = 80;
+//middle ware
+app.use(express.json());
+app.use(express.static('./dist'));
+app.use(compression())
+
+
+app.listen(port, () => {
+  console.log(`server is listening on port ${port}`);
+});
+
+
